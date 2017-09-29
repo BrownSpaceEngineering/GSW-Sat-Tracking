6b811dba7d33f8b25f3ff48847bf798d0d06cbba
Kevin Du <kevin_du@brown.edu>
2017-09-28 22:27:49 -0400

    pyOrbital

--- satTrack.py
+++ satTrack.py
@@ -3,23 +3,44 @@ from datetime import datetime
 from pyorbital.orbital import Orbital
 from getLocation import getLoc
 
-#read in TLE file
-# tle = tlefile.read('ISS', "/home/kevin/Documents/BSE/stations.txt")
-# print("Inclination: " + str(tle.inclination))
-
-#read in TLE for ISS
 satellite = "ISS"
 filepath = "stations.txt"
 orb = Orbital(satellite, filepath)
 now = datetime.utcnow()
-normPos = orb.get_position(now)
-print("Normalized position and velocity of satellite: " + str(normPos))
-lonlatalt = orb.get_lonlatalt(now)
-print("Lon, lat, alt: " + str(lonlatalt))
 myLat = getLoc()[0]
-print("My Latitude: " + str(myLat))
 myLon = getLoc()[1]
-print("My Longitude: " + str(myLon))
+myAlt = 0
 
-angle = orb.get_observer_look(now,myLon, myLat,0)
-print("Angle of elevation: " + str(angle))
+# normPos = orb.get_position(now, normalize=False)
+# print("Normalized position and velocity of satellite: " + str(normPos))
+# print("My Latitude: " + str(myLat))
+# print("My Longitude: " + str(myLon))
+
+#returns velocity of satellite in array (x y z)
+def getVelocity():
+	normPos = orb.get_position(now, normalize=False)
+	vel = normPos[1]
+	print("Velocity vector: " + str(vel))
+	return vel
+
+#returns angle of satellite relative to your position as tuple: (azimuth, elevation)
+def getAngle():
+	angle = orb.get_observer_look(now,myLon, myLat,myAlt)
+	print("Angle of elevation: " + str(angle))
+	return angle
+
+#returns satellite's lon, lat, and alt as tuple: (longitude, latitude, altitude)
+def getSatLonLatAlt():
+	lonlatalt = orb.get_lonlatalt(now)
+	print("Lon, lat, alt: " + str(lonlatalt))
+	return lonlatalt
+
+# def getNextPasses():
+# 	nextPasses = orb.get_next_passes(now, 2, myLon, myLat, myAlt, tol=0.0001, horizon=0)
+# 	print ("Next passes: " + str(nextPasses))
+# 	return nextPasses
+
+getVelocity()
+getAngle()
+getSatLonLatAlt()
+# getNextPasses()
