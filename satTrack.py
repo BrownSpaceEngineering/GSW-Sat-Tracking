5159bbbdf3a438e108c1219756ba95f099bf3d2a
Michael Mao <michael.mao99@gmail.com>
2017-09-29 01:39:25 -0400

    Rewrite some functions to align with documentation

--- satTrack.py
+++ satTrack.py
@@ -2,14 +2,14 @@ from pyorbital import tlefile
 from datetime import datetime
 from pyorbital.orbital import Orbital
 from getLocation import getLoc
+from math import sqrt
 
 satellite = "ISS"
 filepath = "stations.txt"
 orb = Orbital(satellite, filepath)
-now = datetime.utcnow()
-myLat = getLoc()[0]
-myLon = getLoc()[1]
-myAlt = 0
+
+def userLocation(user):
+	return getLoc()
 
 # normPos = orb.get_position(now, normalize=False)
 # print("Normalized position and velocity of satellite: " + str(normPos))
@@ -18,25 +18,42 @@ myAlt = 0
 
 #returns velocity of satellite in array (x y z)
 def getVelocity():
-	normPos = orb.get_position(now, normalize=False)
-	vel = normPos[1]
-	print("Velocity vector: " + str(vel))
-	return vel
+	pass
+	#Need FIXING
 
 #returns angle of satellite relative to your position as tuple: (azimuth, elevation)
-def getAngle():
-	angle = orb.get_observer_look(now,myLon, myLat,myAlt)
-	print("Angle of elevation: " + str(angle))
-	return angle
+def getAzEl():
+	myLat = userLocation("")[0]
+	myLon = userLocation("")[1]
+	myAlt = 0
+	azEl = orb.get_observer_look(getTime(),myLon, myLat,myAlt)
+	return azEl
+
+#returns azimuth and elevation separately
+def getAzimuth():
+	return getAzEl()[0]
+
+def getElevation():
+	return getAzEl()[1]
 
 #returns satellite's lon, lat, and alt as tuple: (longitude, latitude, altitude)
 def getSatLonLatAlt():
-	lonlatalt = orb.get_lonlatalt(now)
-	print("Lon, lat, alt: " + str(lonlatalt))
+	lonlatalt = orb.get_lonlatalt(getTime())
 	return lonlatalt
 
-def returnTime():
-	return now
+#returns sattellite's individual lon, lat and alt
+def getSatLongitude():
+	return getSatLonLatAlt()[0]
+
+def getSatLatitude():
+	return getSatLonLatAlt()[1]
+
+def getSatAltitude():
+	return getSatLonLatAlt()[2]
+
+def getTime():
+	time = datetime.utcnow()
+	return time
 
 # def getNextPasses():
 # 	nextPasses = orb.get_next_passes(now, 2, myLon, myLat, myAlt, tol=0.0001, horizon=0)
@@ -44,7 +61,7 @@ def returnTime():
 # 	return nextPasses
 
 getVelocity()
-getAngle()
+getAzEl()
 getSatLonLatAlt()
-returnTime()
+getTime()
 # getNextPasses()
