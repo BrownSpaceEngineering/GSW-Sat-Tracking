d725bfe6e4ec50f350c522bba0b777714e42e220
Michael Mao <michael.mao99@gmail.com>
2017-10-08 14:24:29 -0400

    getting flask working.
    Needs sudo priviledges to run. Uses python 3.

---
+++ flask.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api')
+def hello_world():
+    return 'Flask running'
+
+@app.route('/api/dummy')
+def dummy():
+    return 'dummy placer'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=80)
